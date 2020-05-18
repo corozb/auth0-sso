1278bf8f3be9cbbe61cb929579ade1258ef0d833
Cristian Orozco <cristianorozco1984@gmail.com>
2020-05-18 12:02:18 -0500

    add user

--- Documents/ReactJS/auth0-sso/src/contexts/auth0-context.js
+++ Documents/ReactJS/auth0-sso/src/contexts/auth0-context.js
@@ -10,6 +10,7 @@ export class Auth0Provider extends Component {
     auth0Client: null,
     isLoading: true,
     isAuthenticated: false,
+    user: null,
   }
 
   config = {
@@ -26,17 +27,19 @@ export class Auth0Provider extends Component {
   initializeAuth0 = async () => {
     const auth0Client = await createAuth0Client(this.config)
     const isAuthenticated = await auth0Client.isAuthenticated()
+    const user = isAuthenticated ? await auth0Client.getUser() : null
 
-    this.setState({ auth0Client, isLoading: false, isAuthenticated })
+    this.setState({ auth0Client, isLoading: false, isAuthenticated, user })
   }
 
   render() {
-    const { isLoading, isAuthenticated } = this.state
+    const { isLoading, isAuthenticated, user } = this.state
     const { children } = this.props
 
     const configObject = {
       isLoading,
       isAuthenticated,
+      user,
     }
 
     return (
